319c2fc97f66d304cc88679868f751b3dd4c712f
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2022-08-23 19:58:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Xml;
+using static System.Console;
+
+namespace SaveInXML
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var userJohn = new User() { Name = "John", Email = "John@john.com" };
+                SerializerXML.SerializeObject<User>(@"C:\Users\ZbooK\source\repos\SaveInXML\MyFile.xml", userJohn);
+                User userJohnReloaded = SerializerXML.DeSerializeObject<User>(@"C:\Users\ZbooK\source\repos\SaveInXML\MyFile.xml");
+                Console.WriteLine(userJohnReloaded.ToString());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+          
+        }
+    }
+    
+}
